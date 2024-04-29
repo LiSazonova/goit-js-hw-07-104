394976418d165a7c1a54f74d93737d0c278074d1
Lili Sazonova <lilissazonova@gmail.com>
2024-04-29 16:54:35 +0300

    add task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,23 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const elements = event.target.elements;
+    const emailInput = elements.email.value.trim();
+    const passwordInput = elements.password.value.trim();
+
+    if (emailInput === '' || passwordInput === '') {
+        return alert('All form fields must be filled in');
+    }
+
+    const formData = {
+        email: emailInput,
+        password: passwordInput
+    }
+
+    console.log(formData);
+
+    form.reset();
+}
