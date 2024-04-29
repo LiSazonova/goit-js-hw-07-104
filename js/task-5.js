8c0898fa3110035dd855e5e420d41c5ec09f5918
Lili Sazonova <lilissazonova@gmail.com>
2024-04-29 17:31:21 +0300

    add task-5

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,17 @@
+const body = document.querySelector('body');
+const btnChange = document.querySelector('.change-color');
+const span = document.querySelector('.color');
+
+btnChange.addEventListener('click', handleClick);
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+    return `#${Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, 0)}`;
+}
+
+function handleClick() {
+    const color = getRandomHexColor();
+    body.style.backgroundColor = color;
+    span.textContent = color;
 }
